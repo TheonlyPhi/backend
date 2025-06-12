0de4158492b4b6c0e5d011425bbe8d8ca69c2792
TheonlyPhi <109999977+TheonlyPhi@users.noreply.github.com>
2025-06-13 00:25:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 5500,
-    host: 'https://oxlskpgoxmnrncflommw.supabase.co',
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
